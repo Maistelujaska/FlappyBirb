0f6f9b46021b83fa3f2672551676ed0744f27c66
Maistelujaska <jaakko.j.rautio@gmail.com>
2025-07-17 10:58:13 +0300

    Initial repository

---
+++ Assets/Scripts/LogicScript.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class LogicScript : MonoBehaviour
+{
+    public int playerScore;
+    public TextMeshProUGUI scoreText;
+    public GameObject gameOverScreen;
+
+    [ContextMenu("Increase Score")]
+    public void addScore(int scoreToAdd)
+    {
+        playerScore = playerScore + scoreToAdd;
+        scoreText.text = playerScore.ToString();
+    }
+
+    public void resetartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void gameOver()
+    {
+        gameOverScreen.SetActive(true);
+    }
+
+
+}
